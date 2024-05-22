066bb8e5059474155c7f2ec5307fbc3638e6a034
DudaSantoni <159277791+DudaSantoni@users.noreply.github.com>
2024-05-22 14:19:06 -0300

    vetores

--- VetoresABC.java
+++ VetoresABC.java
@@ -8,28 +8,30 @@ public class VetoresABC {
 		int v1 [] = new int [5];
 		int v2 [] = new int [5];
 		int v3 [] = new int [10];
-		int soma = 0;
 
 		Scanner ler = new Scanner(System.in);
 
 
-		for (int c =0; c<10; c++) {
-			for (int a =0; a<5; a++) {
-				System.out.println("Informe um valor " + a + ":");
-				v1 [a]= ler.nextInt();
-				v3[c]=v1[a];
-			}
+		for (int i =0; i<5; i++) {
 
-			for (int b =0; b<5; b++) {
-				System.out.println("Informe um valor " + b + ":");
-				v2 [b]= ler.nextInt();
+			System.out.println("Informe um valor " + i + ":");
+			v1[i]= ler.nextInt();
+			v3[i]=v1[i];
 
-			}
+		}
+		for (int i =5; i<10; i++) {
 
-			System.out.println("A soma dos vetores é " + soma);
+			System.out.println("Informe um valor " + i + ":");
+			v2[i]= ler.nextInt();
+			v3[i]=v2[i];
+		}
+
+		for (int i =0; i<10; i++) {
+			System.out.println("A soma dos vetores é " + v3[i]);
 		}
 
 		ler.close();
+
 	}
 }
 
